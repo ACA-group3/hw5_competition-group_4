f21b20b389a3e96830e85c742d0d6e5c1bee2216
RubenGevorgyan <38076066+RubenGevorgyan@users.noreply.github.com>
2021-02-01 23:40:20 +0400

    Add files via upload

--- Testing_Features.py
+++ Testing_Features.py
@@ -1,6 +1,50 @@
 #We should test best Features Here
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+from sklearn.metrics import auc
+import numpy as np
+import pandas as pd
 
-def test():
+# loading dataset
+data = pd.read_csv("Survival_dataset.csv")
+
+
+def ROC_Curve(data,target):
+    x, y = data.loc[:, data.columns != target], data[target]
+    y_ = y == 1
+    plt.figure(figsize=(13, 7))
+    for col in range(x.shape[1]):
+        tpr, fpr = [], []
+        min(x.iloc[:, col])
+        max(x.iloc[:, col])
+
+        for threshold in np.linspace(min(x.iloc[:, col]), max(x.iloc[:, col]), 100):
+            detP = x.iloc[:, col] < threshold
+            tpr.append(sum(detP & y_) / sum(y_))  # TP/P, aka recall
+            fpr.append(sum(detP & (~y_)) / sum((~y_)))  # FP/N
+
+        if auc(fpr, tpr) < .5:
+            aux = tpr
+            tpr = fpr
+            fpr = aux
+        plt.plot(fpr, tpr, label=data.columns[col] + ', auc = ' \
+                                 + str(np.round(auc(fpr, tpr), decimals=3)))
+    plt.title('ROC curve - Iris features')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.legend()
+    plt.show()
     pass
-def test2():
-    pass
+def removeNullColumns(data,target):
+    x, y = data.loc[:, data.columns != target], data[target]
+    for col in x.columns:
+        percetnage = data[col].isnull().sum()/len(data[col])*100
+        if(percetnage>=60):
+            del data[col]
+    return data
+def replaceNullValues(data):
+    for col in data.columns:
+        data[col].fillna(data[col].median())
+    return data
+data = removeNullColumns(data,"In-hospital_death")
+data= replaceNullValues(data)

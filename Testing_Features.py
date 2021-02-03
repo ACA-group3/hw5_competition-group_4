546ec1382ded872625a998590cc74280e79c5f90
RubenGevorgyan <38076066+RubenGevorgyan@users.noreply.github.com>
2021-02-03 08:36:51 +0400

    Added the capability to test through all features, need to finish the model and score part

--- Testing_Features.py
+++ Testing_Features.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import auc
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import itertools
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
 
 # loading dataset
 data = pd.read_csv("Survival_dataset.csv")
@@ -24,9 +29,7 @@ def ROC_Curve(data,target):
             fpr.append(sum(detP & (~y_)) / sum((~y_)))  # FP/N
 
         if auc(fpr, tpr) < .5:
-            aux = tpr
-            tpr = fpr
-            fpr = aux
+            continue
         plt.plot(fpr, tpr, label=data.columns[col] + ', auc = ' \
                                  + str(np.round(auc(fpr, tpr), decimals=3)))
     plt.title('ROC curve - Iris features')
@@ -35,6 +38,27 @@ def ROC_Curve(data,target):
     plt.legend()
     plt.show()
     pass
+
+def tryAllFeatures(data,target):
+    x, y = data.loc[:, data.columns != target], data[target]
+    X_train, X_test, y_train, y_test = train_test_split(x, y)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+    max_value = 0
+    max_columns = itertools
+    clf = svm.SVC(random_state=0)
+    for i in range(X_train.size):
+        columns = itertools.permutations(x.columns,i)
+        new_data=pd.DataFrame
+        for j in columns:
+            temp = 0
+            for k in j:
+                new_data.append(x[k])
+
+            if(temp>max_value):
+                max_columns = columns
+    return max_columns
 def removeNullColumns(data,target):
     x, y = data.loc[:, data.columns != target], data[target]
     for col in x.columns:
@@ -45,6 +69,9 @@ def removeNullColumns(data,target):
 def replaceNullValues(data):
     for col in data.columns:
         data[col].fillna(data[col].median())
+        print(data[col].isnull().sum())
     return data
 data = removeNullColumns(data,"In-hospital_death")
 data= replaceNullValues(data)
+print(data.info())
+tryAllFeatures(data,"In-hospital_death")
